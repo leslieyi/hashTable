d6de2a4f7e91604ddd964548bcc16f78614d3e63
leslieyi <leslieyi411@gmail.com>
2021-12-10 11:40:41 -0500

    medium feels like hard

--- intToRoman.js
+++ intToRoman.js
@@ -1,4 +1,4 @@
-// 13. Roman to Integer EASY
+//12. Integer to Roman
 // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
 // Symbol       Value
@@ -9,7 +9,6 @@
 // C             100
 // D             500
 // M             1000
-
 // For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
@@ -17,67 +16,65 @@
 // I can be placed before V (5) and X (10) to make 4 and 9.
 // X can be placed before L (50) and C (100) to make 40 and 90.
 // C can be placed before D (500) and M (1000) to make 400 and 900.
-// Given a roman numeral, convert it to an integer.
+// Given an integer, convert it to a roman numeral.
 
 // Example 1:
-
-// Input: s = "III"
-// Output: 3
+// Input: num = 3
+// Output: "III"
 
 // Example 2:
-
-// Input: s = "IV"
-// Output: 4
+// Input: num = 4
+// Output: "IV"
 
 // Example 3:
-
-// Input: s = "IX"
-// Output: 9
+// Input: num = 9
+// Output: "IX"
 
 // Example 4:
-
-// Input: s = "LVIII"
-// Output: 58
-// Explanation: L = 50, V= 5, III = 3.
+// Input: num = 58
+// Output: "LVIII"
+// Explanation: L = 50, V = 5, III = 3.
 
 // Example 5:
-
-// Input: s = "MCMXCIV"
-// Output: 1994
+// Input: num = 1994
+// Output: "MCMXCIV"
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 // Constraints:
 
-// 1 <= s.length <= 15
-// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+// 1 <= num <= 3999
+// Accepted
+// 593,343
+// Submissions
+// 1,013,839
 
-function romanToInt(s) {
-  const romanMap = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
+function inToRoman(num) {
+  const map = {
+    //conversions, including the edge cases
     M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
   };
+  //have a var result 
 
-  let result = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    let current = romanMap[s[i]];
-    let next = romanMap[s[i + 1]];
-    if (current < next) {
-      result -= current; 
-    } else result += current; 
-  }
-  return result;
+  return Object.entries(map).reduce((result, [letter, n]) => {
+    result += letter.repeat(Math.floor(num / n)); //moving until division is not equal to zero.
+    num %= n; //stripping off the first digit 2944 -> 944
+    return result;
+  }, "");
 }
+console.log(inToRoman(3));
+console.log(inToRoman(58));
+console.log(inToRoman(1994));
 
-console.log(romanToInt("LVIII"));
-console.log(romanToInt("IV"));
-console.log(romanToInt("IX"));
-console.log(romanToInt("DCXXI"));
-console.log(romanToInt("MCMXCIV"))
-
+//https://www.youtube.com/watch?v=yzB4M-UXqgI

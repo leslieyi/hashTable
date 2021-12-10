8dbce9c994465f35280f9028a31bd5d7e293ca8d
leslieyi <leslieyi411@gmail.com>
2021-12-10 11:14:00 -0500

    woops error is fixed

--- romanToInteger.js
+++ romanToInteger.js
@@ -58,7 +58,8 @@ function romanToInt(s) {
     V: 5,
     X: 10,
     L: 50,
-    C: 500,
+    C: 100,
+    D: 500,
     M: 1000,
   };
 
@@ -78,3 +79,5 @@ function romanToInt(s) {
 console.log(romanToInt("LVIII"));
 console.log(romanToInt("IV"));
 console.log(romanToInt("IX"));
+console.log(romanToInt("DCXXI"));
+
